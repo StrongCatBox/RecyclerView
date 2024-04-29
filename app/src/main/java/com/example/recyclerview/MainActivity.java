b83643356b3bc3ddd4183418d2e1527ed1393077
StrongCatBox <salam-jei@hotmail.fr>
2024-04-29 11:50:57 +0200

    update

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MonAdapter monAdapter;
+    private RecyclerView recyclerView;
+    private List<Items> mesItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.monRecyclerView);
+        mesItems = new ArrayList<>();
+
+        mesItems.add(new Items(R.drawable.logo1,"pokemon","partez dans l'aventure pour attraper les pokemons!"));
+        mesItems.add(new Items(R.drawable.logo2,"BrawlStars","soyez pret pour affronter les joueurs du monde entier avec vos champions!"));
+        mesItems.add(new Items(R.drawable.logo3,"ChickenCross","aider le poulet pour traverser la route!"));
+        mesItems.add(new Items(R.drawable.logo4,"DisneySnap","Capturez des innoubliable cliché grace a vos avatars disney!"));
+        mesItems.add(new Items(R.drawable.logo5,"OnePunchWorld","Incarnez Saitama, completez les quetes et battez les vilains!"));
+        mesItems.add(new Items(R.drawable.logo6,"GamepassXbox","Jouez aux mini jeux de l'ancienne generation de Xbox"));
+        mesItems.add(new Items(R.drawable.logo7,"Minecraft","Construisez et survivez l'aventure avec Steve"));
+        mesItems.add(new Items(R.drawable.logo8,"Akinator","Laissez notre genie Akinator le personnage au quel vous pensez!"));
+        mesItems.add(new Items(R.drawable.logo9,"Doom","Incarnez Doom et plongez dans l'enfer pour combattre le mal"));
+        mesItems.add(new Items(R.drawable.logo10,"Raft","Vous etes seul dans l'ocean avec votre bateau... Survivez l'aventure marine... les dangers des fond marin vous attendent!"));
+
+        monAdapter = new MonAdapter(mesItems); //j'instancie mon Adapter en lui donnant ma liste
+
+       // LinearLayoutManager layoutManager =  new LinearLayoutManager(this);
+       // recyclerView.setLayoutManager(layoutManager);
+
+        int orientation = getResources().getConfiguration().orientation;
+        if (Configuration.ORIENTATION_LANDSCAPE == orientation)
+        {
+            recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 3));
+        }else
+            recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        recyclerView.setAdapter(monAdapter); //j'affiche mon Adapter dans mon recycleView
     }
 }
--- app/src/main/java/com/example/recyclerview/MonAdapter.java
+++ app/src/main/java/com/example/recyclerview/MonAdapter.java
@@ -1,31 +1,63 @@
 package com.example.recyclerview;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MonAdapter extends RecyclerView.Adapter<MonAdapter.MonViewHolder> {
+import java.util.List;
 
+public class MonAdapter extends RecyclerView.Adapter<MonAdapter.MonViewHolder> {
+    List<Items> mesItems;
+
+    public MonAdapter(List<Items> mesItems) {
+        this.mesItems = mesItems;
+    }
 
     @NonNull
     @Override
     public MonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+       // return null;
+        //(inflate:gonfler) je charge ma vue en creant un objet view qui contient mon layout
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.affichage_items, parent,false);
+        return new MonViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MonViewHolder holder, int position) {
 
+        //(holder:titulaire) mise a jour de la vue en cours en fonction de la position
+
+        holder.imageImV.setImageResource(mesItems.get(position).getImage());
+        holder.titreTxtV.setText(mesItems.get(position).getTitre());
+        holder.descriptionTxtV.setText(mesItems.get(position).getDescription());
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        //return 0;
+        return mesItems.size();
     }
 
-    public class MonViewHolder {
+    public class MonViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView titreTxtV, descriptionTxtV;
+        private ImageView imageImV;
 
 
+        public MonViewHolder(@NonNull View itemView) {
+            super(itemView);
+                        //je declare mes itemView lien entre item et xml
+            titreTxtV = itemView.findViewById(R.id.titre);
+            descriptionTxtV = itemView.findViewById(R.id.description);
+            imageImV = itemView.findViewById(R.id.img);
+
+        }
     }
 }
